27ff1ac4cb837ea8db200795c9cd6eb4080d98d7
takeshi-shane <68736862+takeshi-shane@users.noreply.github.com>
2021-04-09 19:13:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -109,6 +109,9 @@ client.on('message', msg => {
 	setTimeout(function() { GetServerPlayers(msg, "193.203.39.13", 'B-Zone (RPG1)') }, 3000)
 	setTimeout(function() { GetServerPlayers(msg, "193.203.39.46", 'B-Hood') }, 4000)
     }
+    else if(command === "testanno") {
+	msg.channel.send({files: ["./images/anno.png"]});    
+    }
 })
 
 function GetServerPlayers(msg, ip, sv_name) {
